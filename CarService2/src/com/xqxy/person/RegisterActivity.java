d22842984e6db6d9ec6fb3aae9a1987ad826860e
Administrator <Administrator@OEM-20140420KVP>
2015-04-14 20:02:20 +0800

    TEST

---
+++ CarService2/src/com/xqxy/person/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.xqxy.person;
+
+import android.os.Bundle;
+
+import com.xqxy.baseclass.BaseActivity;
+import com.xqxy.carservice.R;
+
+public class RegisterActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.person_register);
+	}
+}
