1dd65c869e77f235c79789b457adcdff2454f269
Administrator <Administrator@OEM-20140420KVP>
2015-04-14 22:18:07 +0800

    1

--- CarService2/gen/com/xqxy/carservice/R.java
+++ CarService2/gen/com/xqxy/carservice/R.java
@@ -8,12 +8,15 @@
 package com.xqxy.carservice;
 
 public final class R {
+    public static final class array {
+        public static final int planets=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int more_gray=0x7f040002;
-        public static final int style_color=0x7f040000;
-        public static final int wihte=0x7f040001;
+        public static final int more_gray=0x7f050002;
+        public static final int style_color=0x7f050000;
+        public static final int wihte=0x7f050001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -22,41 +25,53 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int back=0x7f020000;
         public static final int box_disable=0x7f020001;
         public static final int box_normal=0x7f020002;
-        public static final int edit_text_selector=0x7f020003;
-        public static final int finish=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int index_1=0x7f020006;
-        public static final int index_2=0x7f020007;
-        public static final int index_3=0x7f020008;
-        public static final int login_btn=0x7f020009;
-        public static final int login_register=0x7f02000a;
-        public static final int logo=0x7f02000b;
-        public static final int password_ico=0x7f02000c;
-        public static final int radio_noselect=0x7f02000d;
-        public static final int radio_select=0x7f02000e;
-        public static final int radio_selector=0x7f02000f;
-        public static final int username_ico=0x7f020010;
+        public static final int dropdown_ico=0x7f020003;
+        public static final int edit_text_selector=0x7f020004;
+        public static final int error_ico=0x7f020005;
+        public static final int finish=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int index_1=0x7f020008;
+        public static final int index_2=0x7f020009;
+        public static final int index_3=0x7f02000a;
+        public static final int login_btn=0x7f02000b;
+        public static final int login_register=0x7f02000c;
+        public static final int logo=0x7f02000d;
+        public static final int password_ico=0x7f02000e;
+        public static final int radio_noselect=0x7f02000f;
+        public static final int radio_select=0x7f020010;
+        public static final int radio_selector=0x7f020011;
+        public static final int rst_phone=0x7f020012;
+        public static final int username_ico=0x7f020013;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
-        public static final int login_btn=0x7f090005;
-        public static final int login_finish=0x7f090002;
-        public static final int login_forgot=0x7f090006;
-        public static final int login_pwd=0x7f090004;
-        public static final int login_register=0x7f090007;
-        public static final int login_username=0x7f090003;
-        public static final int name=0x7f090008;
-        public static final int rst_lady=0x7f09000a;
-        public static final int rst_man=0x7f090009;
-        public static final int viewPager=0x7f090001;
-        public static final int viewpager=0x7f090000;
+        public static final int action_settings=0x7f0a0014;
+        public static final int login_btn=0x7f0a0005;
+        public static final int login_finish=0x7f0a0002;
+        public static final int login_forgot=0x7f0a0006;
+        public static final int login_pwd=0x7f0a0004;
+        public static final int login_register=0x7f0a0007;
+        public static final int login_username=0x7f0a0003;
+        public static final int rst_brand=0x7f0a000d;
+        public static final int rst_code=0x7f0a000e;
+        public static final int rst_contract=0x7f0a0013;
+        public static final int rst_error=0x7f0a0011;
+        public static final int rst_finish=0x7f0a0008;
+        public static final int rst_getcode=0x7f0a000f;
+        public static final int rst_lady=0x7f0a000b;
+        public static final int rst_man=0x7f0a000a;
+        public static final int rst_name=0x7f0a0009;
+        public static final int rst_phone=0x7f0a000c;
+        public static final int rst_pwd=0x7f0a0010;
+        public static final int rst_register=0x7f0a0012;
+        public static final int viewPager=0x7f0a0001;
+        public static final int viewpager=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -67,12 +82,12 @@ public final class R {
         public static final int person_register=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070002;
     }
     public static final class style {
         /** 
@@ -96,12 +111,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int MyTheme_Basic=0x7f070002;
-        public static final int editStyle=0x7f070003;
+        public static final int AppTheme=0x7f080001;
+        public static final int MyTheme_Basic=0x7f080002;
+        public static final int editStyle=0x7f080003;
     }
 }
