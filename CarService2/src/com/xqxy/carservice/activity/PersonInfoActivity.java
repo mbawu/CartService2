3208ccd515f0046b60bd9a2ccae9738c32211acb
Administrator <Administrator@zhjw-PC>
2015-04-22 22:57:28 +0800

    ddd

--- CarService2/src/com/xqxy/carservice/activity/PersonInfoActivity.java
+++ CarService2/src/com/xqxy/carservice/activity/PersonInfoActivity.java
@@ -1,15 +1,39 @@
 package com.xqxy.carservice.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 import com.xqxy.baseclass.BaseActivity;
 import com.xqxy.carservice.R;
+import com.xqxy.carservice.view.TopTitleView;
 
-public class PersonInfoActivity extends BaseActivity{
-	
+public class PersonInfoActivity extends BaseActivity implements OnClickListener {
+
+	private TopTitleView topTitleView;
+	private ImageView imgHeadPhoto;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.person_info_layout);
+		topTitleView = new TopTitleView(this);
+		imgHeadPhoto = (ImageView) findViewById(R.id.img_person_info_head);
+		findViewById(R.id.text_person_info_car).setOnClickListener(this);
+		findViewById(R.id.layout_person_info_head).setOnClickListener(this);
+		findViewById(R.id.btn_person_info_exit).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.layout_person_info_head:
+			break;
+		case R.id.text_person_info_car:
+			break;
+		case R.id.btn_person_info_exit:
+			break;
+		}
 	}
 }
