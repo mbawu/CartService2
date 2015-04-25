8ad3efd2c2cdd47ff1e1f9286f45a41431e4bfb6
Administrator <Administrator@10.9.50.126>
2015-04-25 17:35:34 +0800

    s

--- CarService2/src/com/xqxy/carservice/activity/CategoryActivity.java
+++ CarService2/src/com/xqxy/carservice/activity/CategoryActivity.java
@@ -1,7 +1,14 @@
 package com.xqxy.carservice.activity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.xqxy.carservice.R;
 import com.xqxy.carservice.view.TopTitleView;
@@ -9,23 +16,44 @@ import com.xqxy.carservice.widget.WheelView;
 import com.xqxy.carservice.widget.adapters.ArrayWheelAdapter;
 
 public class CategoryActivity extends Activity {
-	
+
+	private ListView listView;
 	private TopTitleView topTitleView;
 	private WheelView mViewProvince;
-	protected String[] mProvinceDatas=new String[]{"甘肃1","甘肃2","甘肃3","甘肃4","甘肃5","甘肃6","甘肃7","甘肃8","甘肃9","甘肃10"};
+	protected String[] mProvinceDatas = new String[] { "甘肃1", "甘肃2", "甘肃3",
+			"甘肃4", "甘肃5", "甘肃6", "甘肃7", "甘肃8", "甘肃9", "甘肃10" };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.category_layout);
 		topTitleView = new TopTitleView(this);
 		topTitleView.setTitle("服务分类");
-		
+		listView = (ListView) findViewById(R.id.listview);
 		mViewProvince = (WheelView) findViewById(R.id.id_province);
-		
-		ArrayWheelAdapter ad =new ArrayWheelAdapter<String>(CategoryActivity.this, mProvinceDatas);
+
+		ArrayWheelAdapter ad = new ArrayWheelAdapter<String>(
+				CategoryActivity.this, mProvinceDatas);
 		mViewProvince.setViewAdapter(ad);
 		// 设置可见条目数量
 		mViewProvince.setVisibleItems(7);
-	
+
+		for (int i = 0; i < iconIds.length; i++) {
+			Map map = new HashMap<String, Object>();
+			map.put("img", iconIds[i]);
+			map.put("text", "text" + i);
+			maps.add(map);
+		}
+
+		SimpleAdapter ad1 = new SimpleAdapter(this, maps,
+				R.layout.category_item, new String[] { "img", "text" },
+				new int[] { R.id.img_category_item_icon,
+						R.id.text_category_item_title });
+		listView.setAdapter(ad1);
 	}
+
+	public List<Map<String, Object>> maps = new ArrayList<Map<String, Object>>();
+
+	public int iconIds[] = new int[] { R.drawable.address_car_num,
+			R.drawable.address_name };
 }
--- CarService2/src/com/xqxy/carservice/activity/PersonInfoActivity.java
+++ CarService2/src/com/xqxy/carservice/activity/PersonInfoActivity.java
@@ -1,5 +1,6 @@
 package com.xqxy.carservice.activity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -7,10 +8,11 @@ import android.widget.ImageView;
 
 import com.xqxy.baseclass.BaseActivity;
 import com.xqxy.carservice.R;
+import com.xqxy.carservice.view.PhotoSelectDialog;
 import com.xqxy.carservice.view.TopTitleView;
 
 public class PersonInfoActivity extends BaseActivity implements OnClickListener {
-
+	Dialog dialog;
 	private TopTitleView topTitleView;
 	private ImageView imgHeadPhoto;
 
@@ -23,17 +25,25 @@ public class PersonInfoActivity extends BaseActivity implements OnClickListener 
 		findViewById(R.id.text_person_info_car).setOnClickListener(this);
 		findViewById(R.id.layout_person_info_head).setOnClickListener(this);
 		findViewById(R.id.btn_person_info_exit).setOnClickListener(this);
+		
+		dialog = new PhotoSelectDialog(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.layout_person_info_head:
+			showSelectPhotoDialog();
 			break;
 		case R.id.text_person_info_car:
 			break;
 		case R.id.btn_person_info_exit:
 			break;
 		}
+	}
+
+	public void showSelectPhotoDialog() {
+
+		dialog.show();
 	}
 }
