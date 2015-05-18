ec0edbc2f2d546ad391eeaaa6807bb8eff069b19
Administrator <Administrator@10.9.50.126>
2015-05-18 16:26:32 +0800

    1

--- CarService2/src/com/xqxy/carservice/activity/CategoryActivity.java
+++ CarService2/src/com/xqxy/carservice/activity/CategoryActivity.java
@@ -5,14 +5,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.RadioButton;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.xqxy.baseclass.BaseActivity;
 import com.xqxy.baseclass.MyApplication;
@@ -20,6 +24,7 @@ import com.xqxy.baseclass.NetworkAction;
 import com.xqxy.baseclass.RequestWrapper;
 import com.xqxy.baseclass.ResponseWrapper;
 import com.xqxy.carservice.R;
+import com.xqxy.carservice.adapter.CarBaseAdapter;
 import com.xqxy.carservice.view.TopTitleView;
 import com.xqxy.model.Category;
 import com.xqxy.model.CategoryProduct;
@@ -32,7 +37,8 @@ public class CategoryActivity extends BaseActivity implements
 	private TopTitleView topTitleView;
 
 	private List<Category> categorys;
-	private ArrayAdapter<CategoryProduct> cpAdapter;
+	private CategoryAdapter categoryAdapter;
+	private CategoryProductAdapter cpAdapter;
 	private MyApplication app;
 
 	@Override
@@ -44,11 +50,10 @@ public class CategoryActivity extends BaseActivity implements
 		topTitleView.setTitle("服务分类");
 		listView = (ListView) findViewById(R.id.listview);
 		listViewProduct = (ListView) findViewById(R.id.listview_product);
-
-		cpAdapter = new ArrayAdapter<CategoryProduct>(this,
-				R.layout.category_produc_item);
+		categoryAdapter = new CategoryAdapter(this);
+		cpAdapter = new CategoryProductAdapter(this);
 		listViewProduct.setAdapter(cpAdapter);
-
+		listView.setTag(0);
 		listView.setOnItemClickListener(this);
 		listViewProduct.setOnItemClickListener(this);
 
@@ -61,19 +66,18 @@ public class CategoryActivity extends BaseActivity implements
 	public void onItemClick(AdapterView<?> parent, View view,
 			final int position, long id) {
 		if (parent == listView) {
-			Map<String, Object> map = (HashMap<String, Object>) parent
-					.getItemAtPosition(position);
-
-			listView.requestFocusFromTouch();
-
-			listView.setSelection(position);
-			String cid = map.get("cid").toString();
+			categoryAdapter.setSelectedIndex(position);
+			categoryAdapter.notifyDataSetChanged();
+			String cid = categoryAdapter.getDataList().get(position).getCid()
+					+ "";
 			if (cid != null && !"".equals(cid)) {
 				getCategoryProduct(cid);
 			}
+
 		} else if (parent == listViewProduct) {
 			CategoryProduct cp = cpAdapter.getItem(position);
-
+			cpAdapter.setSelectedIndex(position);
+			cpAdapter.notifyDataSetChanged();
 			Intent intent = new Intent();
 			intent.putExtra("pid", cp.getPid());
 			intent.putExtra("flag", cp.getFlag());
@@ -98,41 +102,18 @@ public class CategoryActivity extends BaseActivity implements
 		if (requestType == NetworkAction.indexF_column) {
 			categorys = responseWrapper.getColumn();
 			if (categorys != null && categorys.size() > 0) {
-				List<Map<String, Object>> dataMaps = new ArrayList<Map<String, Object>>();
-				for (int i = 0, j = 0; i < categorys.size(); i++, j++) {
-					Map map = new HashMap<String, Object>();
-					map.put("cid", categorys.get(i).getCid());
-					map.put("img", iconIds[j]);
-					map.put("text", categorys.get(i).getName());
-					dataMaps.add(map);
-					if (j == iconIds.length - 1) {
-						j = -1;
-					}
-				}
-
-				SimpleAdapter ad1 = new SimpleAdapter(this, dataMaps,
-						R.layout.category_item, new String[] { "img", "text" },
-						new int[] { R.id.img_category_item_icon,
-								R.id.text_category_item_title });
-				listView.setAdapter(ad1);
-
-				getCategoryProduct(dataMaps.get(0).get("cid").toString());
-
-				listView.postDelayed(new Runnable() {
-
-					@Override
-					public void run() {
-						listView.requestFocusFromTouch();
-
-						listView.setSelection(0);
-					}
-				}, 500);
-
+				categoryAdapter.setDataList(categorys);
+				listView.setAdapter(categoryAdapter);
+				getCategoryProduct(categorys.get(0).getCid() + "");
 			}
 		} else if (requestType == NetworkAction.indexF_column_product) {
 			List<CategoryProduct> cps = responseWrapper.getColumn_product();
-			cpAdapter.clear();
-			cpAdapter.addAll(cps);
+			if (cpAdapter.getDataList() != null) {
+				cpAdapter.getDataList().clear();
+			}
+			cpAdapter.setSelectedIndex(0);
+			cpAdapter.addDataList(cps);
+			cpAdapter.notifyDataSetChanged();
 
 		}
 	}
@@ -149,4 +130,103 @@ public class CategoryActivity extends BaseActivity implements
 			R.drawable.category_4, R.drawable.category_5,
 			R.drawable.category_6, R.drawable.category_7, R.drawable.category_8 };
 
+	class CategoryAdapter extends CarBaseAdapter<Category> {
+
+		private int selectedIndex = 0;
+
+		public CategoryAdapter(Activity activity) {
+			super(activity);
+		}
+
+		public int getSelectedIndex() {
+			return selectedIndex;
+		}
+
+		public void setSelectedIndex(int selectedIndex) {
+			this.selectedIndex = selectedIndex;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			ViewHolder viewHolder;
+			if (convertView == null) {
+				convertView = inflater.inflate(R.layout.category_item, null);
+				viewHolder = new ViewHolder();
+				viewHolder.nameText = (TextView) convertView
+						.findViewById(R.id.text_category_item_title);
+				viewHolder.imgView = (ImageView) convertView
+						.findViewById(R.id.img_category_item_icon);
+
+				convertView.setTag(viewHolder);
+
+			} else {
+				viewHolder = (ViewHolder) convertView.getTag();
+			}
+
+			if (selectedIndex == position) {
+				convertView
+						.setBackgroundResource(R.drawable.category_item_border_selected);
+				viewHolder.nameText.setTextColor(getResources().getColor(
+						R.color.wihte));
+			} else {
+				convertView
+						.setBackgroundResource(R.drawable.category_item_normal);
+				viewHolder.nameText.setTextColor(getResources().getColor(
+						R.color.blank));
+			}
+			Category c = getDataList().get(position);
+			viewHolder.nameText.setText(c.getName());
+			viewHolder.imgView.setImageResource(iconIds[position
+					% iconIds.length]);
+			return convertView;
+
+		}
+	}
+
+	class CategoryProductAdapter extends CarBaseAdapter<CategoryProduct> {
+
+		private int selectedIndex = 0;
+
+		public CategoryProductAdapter(Activity activity) {
+			super(activity);
+		}
+
+		public int getSelectedIndex() {
+			return selectedIndex;
+		}
+
+		public void setSelectedIndex(int selectedIndex) {
+			this.selectedIndex = selectedIndex;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView textView;
+			if (convertView == null) {
+				convertView = inflater.inflate(R.layout.category_produc_item,
+						null);
+
+			}
+			textView = (TextView) convertView;
+			if (selectedIndex == position) {
+				textView.setBackgroundResource(R.drawable.category_item_border_selected);
+				textView.setTextColor(getResources().getColor(R.color.wihte));
+			} else {
+				textView.setBackgroundColor(getResources().getColor(
+						R.color.wihte));
+				textView.setTextColor(getResources().getColor(R.color.blank));
+			}
+			CategoryProduct cp = getDataList().get(position);
+			textView.setText(cp.getName());
+
+			return convertView;
+
+		}
+	}
+
+	class ViewHolder {
+		ImageView imgView;
+		TextView nameText;
+	}
 }
