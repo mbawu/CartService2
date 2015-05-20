e89ebd983d86f460838365c135948ee7b037e0ed
ChangHong_XXX <ChangHong_XXX@10.9.50.84>
2015-05-20 20:52:09 +0800

    增加储值卡和增值卡的选择

--- CarService2/src/com/xqxy/person/OrderPayActivity.java
+++ CarService2/src/com/xqxy/person/OrderPayActivity.java
@@ -1,5 +1,6 @@
 package com.xqxy.person;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,6 +38,7 @@ import com.xqxy.model.Category;
 import com.xqxy.model.Coupon;
 import com.xqxy.model.OrderProduct;
 import com.xqxy.model.PayModel;
+import com.xqxy.model.PayType;
 import com.xqxy.model.StoreCard;
 import com.xqxy.model.UserInfo;
 
@@ -44,6 +46,8 @@ public class OrderPayActivity extends BaseActivity implements
 		OnCheckedChangeListener {
 
 	public static int GET_COUPON = 1001;
+	public static int GET_CLEANCARD = 1002;
+	public static int GET_STORECARD = 1003;
 	private ImageView backImageView;
 	private TextView titleTextView;
 	private TextView rightBtnTextView;
@@ -54,6 +58,11 @@ public class OrderPayActivity extends BaseActivity implements
 	private double total = 0.0;
 	private double acTotal = 0.0;
 	private double creditePrice = 0.0;
+	private double creditePriceUse = 0.0;// 实际消耗的积分金额
+	private double couponPriceUse = 0.0;// 实际消耗的优惠金额
+	private double cleancardPriceUse = 0.0;
+	private double storecardPriceUse = 0.0;
+
 	private TextView priceTxt;
 	private TextView priceAcTxt;
 	private int payMethod = 1;// 1.支付宝客户端 2.支付宝网页 3. 线下支付
@@ -64,6 +73,7 @@ public class OrderPayActivity extends BaseActivity implements
 	private String oid;
 	private LinearLayout couponLayout;
 	private TextView couponSelect;
+	private TextView couponNum;
 	private LinearLayout storecardLayout;
 	private TextView storecardSelect;
 	private TextView storecardNum;
@@ -75,13 +85,24 @@ public class OrderPayActivity extends BaseActivity implements
 	private View line3;
 	private ArrayList<Coupon> datas;
 	private ArrayList<StoreCard> cardDatas;
+	private ArrayList<Coupon> couponData;
 	private ArrayList<StoreCard> cleancardDatas;
 	private ArrayList<StoreCard> storecardDatas;
 	private CheckBox coupon;
-	private CheckBox storecard;
+	private CheckBox storecard_cb;
+	private CheckBox cleancard_cb;
 	private CheckBox credite;
 	private Coupon coupon2 = null;
+	private StoreCard cleanCard = null;
+	private StoreCard storeCard = null;
 	private Address address;
+	private boolean couponB = false;
+	private boolean storecardB = false;
+	private boolean cleancardB = false;
+	private boolean crediteB = false;
+	private boolean offlineB = false;
+	private Cart cleancart; //使用储值卡的服务
+	private ArrayList<Cart> storecardCarts;//使用增值卡的服务集合
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -102,24 +123,27 @@ public class OrderPayActivity extends BaseActivity implements
 		listView = (ListView) findViewById(R.id.listview);
 		priceTxt = (TextView) findViewById(R.id.order_price_txt);
 		priceAcTxt = (TextView) findViewById(R.id.order_total_price_txt);
-		couponLayout = (LinearLayout) findViewById(R.id.order_coupon_layout);
-		couponSelect = (TextView) findViewById(R.id.select_coupon);
+
 		carts = (ArrayList<Cart>) getIntent().getSerializableExtra("data");
-		address =(Address) getIntent().getSerializableExtra("address");
+		address = (Address) getIntent().getSerializableExtra("address");
 		oid = getIntent().getStringExtra("oid");
 		setPrice();
 
 		cleancardDatas = new ArrayList<StoreCard>();
 		storecardDatas = new ArrayList<StoreCard>();
+		couponData = new ArrayList<Coupon>();
 		storecardLayout = (LinearLayout) findViewById(R.id.order_storecard_layout);
 		storecardSelect = (TextView) findViewById(R.id.select_storecard);
 		storecardNum = (TextView) findViewById(R.id.storecard_num);
 		clearcardLayout = (LinearLayout) findViewById(R.id.order_cleancard_layout);
-		clearcardSelect = (TextView) findViewById(R.id.cleancard_num);
-		clearcardNum = (TextView) findViewById(R.id.select_cleancard);
-		line1=findViewById(R.id.coupon_line);
-		line2=findViewById(R.id.storecard_line);
-		line3=findViewById(R.id.cleancard_line);
+		clearcardNum = (TextView) findViewById(R.id.cleancard_num);
+		clearcardSelect = (TextView) findViewById(R.id.select_cleancard);
+		couponLayout = (LinearLayout) findViewById(R.id.order_coupon_layout);
+		couponSelect = (TextView) findViewById(R.id.select_coupon);
+		couponNum = (TextView) findViewById(R.id.coupon_num);
+		line1 = findViewById(R.id.coupon_line);
+		line2 = findViewById(R.id.storecard_line);
+		line3 = findViewById(R.id.cleancard_line);
 		adapter = new CartAdapter(this);
 		adapter.setDataList(carts);
 		listView.setAdapter(adapter);
@@ -137,14 +161,16 @@ public class OrderPayActivity extends BaseActivity implements
 		alipayWeb = (RadioButton) findViewById(R.id.alipay_web);
 		offline = (RadioButton) findViewById(R.id.offline);
 		coupon = (CheckBox) findViewById(R.id.coupon);
-		storecard = (CheckBox) findViewById(R.id.storecard);
+		storecard_cb = (CheckBox) findViewById(R.id.storecard_cb);
+		cleancard_cb = (CheckBox) findViewById(R.id.cleancard_cb);
 		credite = (CheckBox) findViewById(R.id.credite);
 		alipayClient.setOnCheckedChangeListener(this);
 		alipayWeb.setOnCheckedChangeListener(this);
 		offline.setOnCheckedChangeListener(this);
 		credite.setOnCheckedChangeListener(this);
 		coupon.setOnCheckedChangeListener(this);
-		storecard.setOnCheckedChangeListener(this);
+		storecard_cb.setOnCheckedChangeListener(this);
+		cleancard_cb.setOnCheckedChangeListener(this);
 		couponSelect.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -152,8 +178,36 @@ public class OrderPayActivity extends BaseActivity implements
 				Intent intent = new Intent();
 				intent.setClass(OrderPayActivity.this, CouponActivity.class);
 				intent.putExtra("select", "select");
+				intent.putExtra("data", (Serializable) couponData);
 				OrderPayActivity.this
 						.startActivityForResult(intent, GET_COUPON);
+			}
+		});
+
+		clearcardSelect.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClass(OrderPayActivity.this, StoreCardActivity.class);
+				intent.putExtra("select", "1");
+				intent.putExtra("data", (Serializable) cleancardDatas);
+				OrderPayActivity.this.startActivityForResult(intent,
+						GET_CLEANCARD);
+
+			}
+		});
+		storecardSelect.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClass(OrderPayActivity.this, StoreCardActivity.class);
+				intent.putExtra("select", "2");
+				intent.putExtra("data", (Serializable) storecardDatas);
+				OrderPayActivity.this.startActivityForResult(intent,
+						GET_STORECARD);
+
 			}
 		});
 		getCoupon();
@@ -181,29 +235,212 @@ public class OrderPayActivity extends BaseActivity implements
 		super.onActivityResult(requestCode, resultCode, data);
 		if (resultCode == GET_COUPON) {
 			coupon2 = (Coupon) data.getSerializableExtra("data");
-			if (coupon2.getName().equals(""))
-				coupon.setText("优惠券" + coupon2.getPrice() + "元");
-			else
-				coupon.setText(coupon2.getName() + coupon2.getPrice() + "元");
-
-			getNewPrice();
+			coupon.setText(coupon2.getPrice() + "元" + coupon2.getName() + "优惠券");
+			coupon.setChecked(true);
+		} else if (resultCode == GET_CLEANCARD) {
+			cleanCard = (StoreCard) data.getSerializableExtra("data");
+			cleancard_cb.setText(cleanCard.getName());
+			cleancard_cb.setChecked(true);
+		} else if (resultCode == GET_STORECARD) {
+			storeCard = (StoreCard) data.getSerializableExtra("data");
+			storecard_cb.setText(storeCard.getName());
+			storecard_cb.setChecked(true);
 		}
 	}
 
-	private void getNewPrice() {
+	private void getNewPrice(Object ob) {
 
-		if (coupon2 != null) {
-			if (coupon.isChecked())
-				acTotal = total - Double.valueOf(coupon2.getPrice())
-						- creditePrice;
-			else
-				acTotal = total - creditePrice;
+		// 选择优惠券以后
+		if (ob instanceof Coupon) {
+			coupon.setVisibility(View.VISIBLE);
+			if (coupon.isChecked()) {
+				if (offlineB) {
+					couponB = false;
+					coupon.setChecked(false);
+					Toast.makeText(this, "线下支付不可使用积分和优惠券", Toast.LENGTH_SHORT)
+							.show();
+					return;
+				}
+				if (storecardB || cleancardB) {
+					couponB = false;
+					coupon.setChecked(false);
+					Toast.makeText(this, "储值卡和增值卡不可与积分和优惠券同时使用",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+				couponB = true;
+				// acTotal = acTotal - Double.valueOf(coupon2.getPrice());
+				checkPrice(Double.valueOf(coupon2.getPrice()), PayType.优惠券,
+						coupon.isChecked());
+			} else {
+				if (couponB) {
+					couponB = false;
+					checkPrice(Double.valueOf(coupon2.getPrice()), PayType.优惠券,
+							coupon.isChecked());
+				}
+			}
+
+		} else if (ob instanceof Double) {
+			if (credite.isChecked()) {
+				if (offlineB) {
+					crediteB = false;
+					credite.setChecked(false);
+					Toast.makeText(this, "线下支付不可使用积分和优惠券", Toast.LENGTH_SHORT)
+							.show();
+					return;
+				}
+				if (storecardB || cleancardB) {
+					crediteB = false;
+					credite.setChecked(false);
+					Toast.makeText(this, "储值卡和增值卡不可与积分和优惠券同时使用",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+				crediteB = true;
+				checkPrice(creditePrice, PayType.积分, credite.isChecked());
+			} else {
+				if (crediteB) {
+					crediteB = false;
+					checkPrice(creditePrice, PayType.积分, credite.isChecked());
+				}
+
+			}
+		} else if (ob instanceof StoreCard) {
+			// cleanCard
+			if (((StoreCard) ob).getFlag().equals("1")) {
+				cleancard_cb.setVisibility(View.VISIBLE);
+				if (cleancard_cb.isChecked()) {
+					if (crediteB || couponB) {
+						cleancardB = false;
+						cleancard_cb.setChecked(false);
+						Toast.makeText(this, "储值卡和增值卡不可与积分和优惠券同时使用",
+								Toast.LENGTH_SHORT).show();
+						return;
+					}
+					cleancardB = true;
+					if (cleancart == null) {
+						for (int i = 0; i < carts.size(); i++) {
+							Cart cartTemp = carts.get(i);
+							if (cartTemp.getPid().equals(cleanCard.getPid()))
+								cleancart = cartTemp;
+						}
+					}
+					checkPrice(Double.valueOf(cleancart.getReal_price()),
+							PayType.储值卡, cleancard_cb.isChecked());
+				} else {
+					if (cleancardB) {
+						cleancardB = false;
+						checkPrice(Double.valueOf(cleancart.getReal_price()), PayType.储值卡,
+								cleancard_cb.isChecked());
+					}
+				}
+			}
+			// storeCard
+			else if (((StoreCard) ob).getFlag().equals("2")) {
+				storecard_cb.setVisibility(View.VISIBLE);
+				if (storecard_cb.isChecked()) {
+					if (crediteB || couponB) {
+						storecardB = false;
+						storecard_cb.setChecked(false);
+						Toast.makeText(this, "储值卡和增值卡不可与积分和优惠券同时使用",
+								Toast.LENGTH_SHORT).show();
+						return;
+					}
+					storecardB = true;
+					if (cleancart == null) {
+						for (int i = 0; i < carts.size(); i++) {
+							Cart cartTemp = carts.get(i);
+							if (cartTemp.getPid().equals(cleanCard.getPid()))
+								cleancart = cartTemp;
+						}
+					}
+					checkPrice(Double.valueOf(cleancart.getReal_price()),
+							PayType.储值卡, cleancard_cb.isChecked());
+				} else {
+					if (cleancardB) {
+						cleancardB = false;
+						checkPrice(Double.valueOf(cleancart.getReal_price()), PayType.储值卡,
+								cleancard_cb.isChecked());
+					}
+				}
+			}
+		}
+
+	}
+
+	private void checkPrice(double price, PayType type, boolean ischeck) {
+		double temp = acTotal;
+		if (type == PayType.积分) {
+
+			if (ischeck) {
+				if (acTotal <= 0) {
+					Toast.makeText(this, "无需使用积分了", Toast.LENGTH_SHORT).show();
+					credite.setChecked(false);
+					return;
+				}
+
+				temp = acTotal - price;
+				if (temp <= 0) {
+					creditePriceUse = price - acTotal;
+				} else {
+					creditePriceUse = price;
+				}
+				acTotal = acTotal - price;
+			} else {
+				if (creditePriceUse == 0.0)
+					return;
+				acTotal = acTotal + price;
+				creditePriceUse = 0.0;
+			}
+		} else if (type == PayType.优惠券) {
+
+			if (ischeck) {
+				if (acTotal <= 0) {
+					Toast.makeText(this, "无需使用优惠券了", Toast.LENGTH_SHORT).show();
+					coupon.setChecked(false);
+					return;
+				}
+
+				temp = acTotal - price;
+				if (temp <= 0) {
+					couponPriceUse = acTotal;
+				} else {
+					couponPriceUse = price;
+				}
+				acTotal = acTotal - price;
+			} else {
+				if (couponPriceUse == 0.0)
+					return;
+				acTotal = acTotal + price;
+				couponPriceUse = 0.0;
+			}
+		} else if (type == PayType.储值卡) {
+			if (ischeck) {
+				if (acTotal <= 0) {
+					Toast.makeText(this, "无需使用储值卡了", Toast.LENGTH_SHORT).show();
+					cleancard_cb.setChecked(false);
+					cleancardB = false;
+					return;
+				}
+				cleancardPriceUse = price;
+				acTotal = acTotal - price;
+			} else {
+				if (cleancardPriceUse == 0.0)
+					return;
+				acTotal = acTotal + price;
+				couponPriceUse = 0.0;
+			}
 
 		}
+
 		if (acTotal > 0)
 			priceAcTxt.setText("￥" + acTotal);
 		else
-			priceAcTxt.setText("￥0.0");
+			priceAcTxt.setText("￥" + "0.0");
+
+		Log.i("test", "实际使用积分：" + creditePriceUse * 10);
+		Log.i("test", "实际优惠金额：" + couponPriceUse);
+		Log.i("test", "实际储值卡扣减金额：" + cleancardPriceUse);
 	}
 
 	private void payOrder() {
@@ -254,22 +491,43 @@ public class OrderPayActivity extends BaseActivity implements
 			Toast.makeText(this, "获取信息失败，请重试", Toast.LENGTH_SHORT).show();
 		} else if (requestType == NetworkAction.centerF_user_coupon) {
 			datas = responseWrapper.getCoupon();
-			if (datas.size() > 0) {
-				coupon2 = datas.get(0);
-				line1.setVisibility(View.VISIBLE);
+
+			for (int i = 0; i < datas.size(); i++) {
+				Coupon coupon = datas.get(i);
+				for (int j = 0; j < carts.size(); j++) {
+					Cart cart = carts.get(j);
+					if (cart.getPid().equals(coupon.getPid())) {
+						if (couponData.size() > 0) {
+							for (int k = 0; k < couponData.size(); k++) {
+								Coupon couponTemp = datas.get(k);
+								if (!couponTemp.getCid()
+										.equals(coupon.getCid()))
+									couponData.add(coupon);
+							}
+						} else
+							couponData.add(coupon);
+					}
+				}
+			}
+
+			if (couponData.size() > 0) {
 				couponLayout.setVisibility(View.VISIBLE);
-				coupon.setVisibility(View.VISIBLE);
-				if (coupon2.getName().equals(""))
-					coupon.setText("优惠券" + coupon2.getPrice() + "元");
-				else
-					coupon.setText(coupon2.getName() + coupon2.getPrice() + "元");
-				getNewPrice();
-			} else
-			{
+				couponNum.setText(getString(R.string.coupon_use,
+						couponData.size() + ""));
+			} else {
 				couponLayout.setVisibility(View.GONE);
 				line1.setVisibility(View.GONE);
 			}
-				
+			// coupon2 = datas.get(0);
+			// line1.setVisibility(View.VISIBLE);
+			// couponLayout.setVisibility(View.VISIBLE);
+			// coupon.setVisibility(View.VISIBLE);
+			// if (coupon2.getName().equals(""))
+			// coupon.setText("优惠券" + coupon2.getPrice() + "元");
+			// else
+			// coupon.setText(coupon2.getName() + coupon2.getPrice() + "元");
+			// getNewPrice();
+
 		} else if (requestType == NetworkAction.centerF_user) {
 			UserInfo user = responseWrapper.getUser();
 			creditePrice = Double.valueOf(user.getIntegral()) / 10;
@@ -287,11 +545,13 @@ public class OrderPayActivity extends BaseActivity implements
 							Cart cart = carts.get(j);
 							// 储值卡的服务ID和购买的服务ID相符，并且地址上的车牌号和购买储值卡时候的车牌号要相符
 							if (cart.getPid().equals(card.getPid())
-									&& address.getCar_num().equals(card.getCar_num()) ) {
+									&& address.getCar_num().equals(
+											card.getCar_num())) {
 
 								if (cleancardDatas.size() > 1) {
 									for (int k = 0; k < cleancardDatas.size(); k++) {
-										StoreCard cardTemp = cleancardDatas.get(k);
+										StoreCard cardTemp = cleancardDatas
+												.get(k);
 										if (!cardTemp.getCid().equals(
 												card.getCid())) {
 											cleancardDatas.add(card);
@@ -306,84 +566,102 @@ public class OrderPayActivity extends BaseActivity implements
 					// 增值卡
 					else if (card.getFlag().equals("2")) {
 
-						//先判断是否是全场使用的
-						if(card.getColid().equals("0"))
-						{
+						// 先判断是否是全场使用的
+						if (card.getColid().equals("0")) {
 							storecardDatas.add(card);
-						}
-						else
-						{
+						} else {
 							for (int j = 0; j < carts.size(); j++) {
 								Cart cart = carts.get(j);
-								if (cart.getPid().equals(card.getPid()))
-								{
+								if (cart.getPid().equals(card.getPid())) {
 									if (storecardDatas.size() > 1) {
-										for (int k = 0; k < storecardDatas.size(); k++) {
-											StoreCard cardTemp = storecardDatas.get(k);
+										for (int k = 0; k < storecardDatas
+												.size(); k++) {
+											StoreCard cardTemp = storecardDatas
+													.get(k);
 											if (!cardTemp.getCid().equals(
 													card.getCid())) {
 												storecardDatas.add(card);
 											}
 										}
-									}
-									else
-									{
+									} else {
 										storecardDatas.add(card);
 									}
 								}
 							}
 						}
-					
+
 					}
 				}
 
 			}
-			
-			if(cleancardDatas.size()>0)
-			{
+
+			if (cleancardDatas.size() > 0) {
 				clearcardLayout.setVisibility(View.VISIBLE);
-				clearcardNum.setText(getString(R.string.cleancard_use,cleancardDatas.size()+""));
-			}
-			else
-			{
+				clearcardNum.setText(getString(R.string.cleancard_use,
+						cleancardDatas.size() + ""));
+			} else {
 				clearcardLayout.setVisibility(View.GONE);
 				line3.setVisibility(View.GONE);
 			}
-				
-			
-			if(storecardDatas.size()>0)
-			{
+
+			if (storecardDatas.size() > 0) {
 				storecardLayout.setVisibility(View.VISIBLE);
-				storecardNum.setText(getString(R.string.storecard_num,storecardDatas.size()+""));
-			}
-			else
-			{
+				storecardNum.setText(getString(R.string.sotrecard_use,
+						storecardDatas.size() + ""));
+			} else {
 				storecardLayout.setVisibility(View.GONE);
 				line2.setVisibility(View.GONE);
 			}
-			
 		}
 	}
 
 	@Override
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		getNewPrice();
-		if (isChecked) {
-			switch (buttonView.getId()) {
-			case R.id.alipay_client:
-				payMethod = 1;
-				break;
-			case R.id.alipay_web:
-				payMethod = 2;
-				break;
-			case R.id.offline:
-				payMethod = 3;
-				break;
-			case R.id.credite:
-				acTotal = acTotal - creditePrice;
-				priceAcTxt.setText("￥" + acTotal);
-				break;
+		// getNewPrice();
+		// if (isChecked) {
+		switch (buttonView.getId()) {
+		case R.id.alipay_client:
+			payMethod = 1;
+			break;
+		case R.id.alipay_web:
+			payMethod = 2;
+			break;
+		case R.id.offline:
+			payMethod = 3;
+			offlineB = isChecked;
+			if (isChecked) {
+				coupon.setChecked(false);
+				credite.setChecked(false);
 			}
+			break;
+		case R.id.credite:
+			// acTotal = acTotal - creditePrice;
+			// priceAcTxt.setText("￥" + acTotal);
+			getNewPrice(creditePrice);
+			break;
+		case R.id.coupon:
+			if (coupon2 != null)
+				getNewPrice(coupon2);
+			break;
+		case R.id.storecard_cb:
+			// if (storeCard != null)
+			if (isChecked) {
+				storecardB = true;
+				coupon.setChecked(false);
+				credite.setChecked(false);
+			} 
+			getNewPrice(storeCard);
+			break;
+		case R.id.cleancard_cb:
+			// if (cleanCard != null)
+			if (isChecked) {
+				cleancardB = true;
+				coupon.setChecked(false);
+				credite.setChecked(false);
+			} 
+			getNewPrice(cleanCard);
+			break;
+		// }
 		}
 
 	}
@@ -393,7 +671,9 @@ public class OrderPayActivity extends BaseActivity implements
 			Cart cart = carts.get(i);
 			total = total + Double.valueOf(cart.getReal_price());
 		}
+		acTotal = total;
 		priceTxt.setText("￥" + total);
+		priceAcTxt.setText("￥" + total);
 	}
 
 	class CartAdapter extends CarBaseAdapter<Cart> {
